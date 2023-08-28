1d0af83e617e4151fba6e107375f7c3dc6502af9
Mai Tiến Đức -Ph20223 <124181138+dragonkingl6@users.noreply.github.com>
2023-08-28 09:54:31 +0700

    Check create Account firebase

--- app/src/main/java/com/example/journalapp/MainActivity.kt
+++ app/src/main/java/com/example/journalapp/MainActivity.kt
@@ -1,15 +1,26 @@
 package com.example.journalapp
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.databinding.DataBindingUtil
+import com.example.journalapp.databinding.ActivityMainBinding
+import com.example.journalapp.databinding.ActivitySignUpBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : ComponentActivity() {
 
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main) /// checkout
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.btnCreate.setOnClickListener(){
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/journalapp/SignUpActivity.kt
+++ app/src/main/java/com/example/journalapp/SignUpActivity.kt
@@ -1,19 +1,73 @@
 package com.example.journalapp
 
 import android.app.Activity
+import android.nfc.Tag
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
 import com.example.journalapp.databinding.ActivitySignUpBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 class SignUpActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivitySignUpBinding
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_sign_up)
+        auth = Firebase.auth
+        binding.btnSignUp.setOnClickListener() {
+            createUser()
+        }
 
     }
 
+    private fun createUser() {
+
+        val email = binding.editTextSignEmail.text.toString()
+        val password = binding.editTextSignPassword.text.toString()
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d("Tag", "createUserWithEmail:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w("Tag", "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(
+                        baseContext,
+                        "Authentication failed.",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                    updateUI(null)
+                }
+            }
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            reload()
+        }
+
+
+    }
+
+    public fun reload() {
+
+    }
 }
